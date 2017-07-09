f1e369f6b0983a4820452b12fe7126cf086b7de7
Jacob Knott <sam41298@gmail.com>
2017-07-09 19:28:20 -0400

    Second commit

--- chapter 3/Book instantiator/Book.java
+++ chapter 3/Book instantiator/Book.java
@@ -41,6 +41,48 @@ public class Book
         }
     }
     
+    //THis method allows us to change all details of a book object
+    public void setBookDetails(String title, String firstName, String lastName,int pages, String isbn, 
+    String datePublished){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.isbn = isbn;
+        this.title = title;
+        pubDate = datePublished;
+        if(pages < 10){
+          System.out.println( "Books must be more than 10 pages long. Please try again");  
+          
+        }
+        else {
+          this.pages = pages;
+        }
+    }
+    
+    //changes the title
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    //changes the first name
+    public void changeFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    //changes the last name
+    public void changeLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+      //changes the isbn name
+    public void changeISBN(String isbn){
+        this.isbn = isbn;
+    }
+    
+     //changes the isbn name
+    public void changeDatePublished(String pubDate){
+        this.pubDate = pubDate;
+    }
+    
     // Changes the numeber of pages in the Book
     public void editNumberOfPages(int page)
     {
@@ -51,5 +93,41 @@ public class Book
             pages = page;
         }
     }
+   
+    // Print author's full Name
+    public void printAuthorName(){
+        System.out.println( "Author: " + firstName + lastName);
+    }
     
+    // Print the books title
+    public void printTitle(){
+        System.out.println( "Title: " + title);
+    }
+    
+    // Print number of pages
+    public void printPageCount(){
+        System.out.println( "Number of Pages: " + pages);
+    }
+    
+    // Print the books ISBN #
+    public void printISBN(){
+        System.out.println( "ISBN: " + isbn);
+    }
+    
+    // Print the date published
+    public void printDatePublished(){
+        System.out.println( "Date Published: " + pubDate);
+    }
+    
+    // Print Books Detials
+    public void printBookDetails(){
+        System.out.println();
+        System.out.println( "Book Details");
+        System.out.println( "Title: " + title);
+        System.out.println( "Author: " + firstName + lastName);
+        System.out.println( "Number of Pages: " + pages);
+        System.out.println( "ISBN: " + isbn);
+        System.out.println( "Date Published: " + pubDate);
+        System.out.println();
+    }
 }
--- chapter 3/Book instantiator/BookInterface.java
+++ chapter 3/Book instantiator/BookInterface.java
@@ -1,11 +1,63 @@
 
 /**
- * Write a description of class BookInterface here.
+ * This allows the user to interact with the book object
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Jacob Knott)
  */
 public class BookInterface
 {
     private Book book;
+    
+    /**
+     * Creates and allows interaction with an individual book object
+	 *
+     */
+    public BookInterface()
+    {
+        book = new Book("unknown", "unknown", "unknown", "unknown","unknown", 0
+        );
+    }
+    
+/**
+*  Use this method to give the book object details. 
+*/
+    public void setBookDetials( String lastName, String firstName, String isbn, String title, 
+    String datePublished, int pages){
+        book.changeTitle(title);
+		book.changeFirstName(firstName);
+        book.changeLastName(lastName);
+		book.changeISBN(isbn);
+		book.changeDatePublished(datePublished);
+		book.editNumberOfPages(pages);
+    }  
+
+	//Method calls printDetials
+	public void printBookDetails(){
+	book.printBookDetails();
+	}
+
+	//Method calls printTitle
+	public void printTitle(){
+	 System.out.println();
+	}	
+	
+	//Method calls printDetials
+	public void printAuthorName(){
+	book.printAuthorName();
+	}
+
+	//this method calls printNumberOfPages
+	public void printNumberOfPages(){
+	book.printPageCount();
+	}
+
+	//This method calls and displays the ISBN #
+	public void printISBN(){
+	book.printISBN();
+	}
+
+	//this prints the date it was published
+	public void prindPublishmentDate(){
+	book.printDatePublished();
+	}
 }
